5a9ada3c0e5ee5b74b1366d739d8a615e08afe4f
cataafra <afr.catalin@gmail.com>
2023-04-11 13:02:54 +0300

    .

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const PROD_API_URL = "http://ec2-16-16-75-13.eu-north-1.compute.amazonaws.com/";
 const LOCAL_API_URL = "http://127.0.0.1:8000/";
-const NETLIFY_URL = "/api/";
+const NETLIFY_URL = "http://16.170.29.78/";
 
 const ApiService = (url) => {
   return {
