c159e9c777207957abbc1306f7060de34162f50d
cataafra <afr.catalin@gmail.com>
2023-05-17 13:41:37 +0300

    comm

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,31 +1,41 @@
 import axios from "axios";
-
 const PROD_API_URL =
   "https://ec2-16-170-29-78.eu-north-1.compute.amazonaws.com/";
 const LOCAL_API_URL = "http://127.0.0.1:8000/";
 const IP_URL = "http://16.170.29.78";
 const AFRA_URL = "https://api.afrastudios.xyz/";
 
-const ApiService = (url) => {
+const ApiService = (url, token = "") => {
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+
   return {
     get(endpoint) {
-      return axios.get(url + endpoint).then((response) => response.data);
+      return axios
+        .get(url + endpoint, { headers })
+        .then((response) => response.data);
     },
 
     post(endpoint, data) {
-      return axios.post(url + endpoint, data).then((response) => response.data);
+      return axios
+        .post(url + endpoint, data, { headers })
+        .then((response) => response.data);
     },
 
     patch(endpoint, data) {
       return axios
-        .patch(url + endpoint, data)
+        .patch(url + endpoint, data, { headers })
         .then((response) => response.data);
     },
 
     delete(endpoint) {
-      return axios.delete(url + endpoint).then((response) => response.data);
+      return axios
+        .delete(url + endpoint, { headers })
+        .then((response) => response.data);
     },
   };
 };
 
-export default ApiService(AFRA_URL);
+export default ApiService;
